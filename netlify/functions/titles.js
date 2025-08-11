788d009f4d7bec8945718dc2446c4888a431af42
NuoNuo720 <3668417644@qq.com>
2025-08-11 14:30:58 +0800

    a

--- netlify/functions/titles.js
+++ netlify/functions/titles.js
@@ -1,16 +1,11 @@
+// 极简测试函数，仅返回状态码200
 exports.handler = async (event) => {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json"
-  };
-
-  // 模拟返回两个称号
   return {
     statusCode: 200,
-    headers,
-    body: JSON.stringify([
-      { id: "001", name: "测试称号1", description: "模拟数据", icon: "star" },
-      { id: "002", name: "测试称号2", description: "模拟数据", icon: "users" }
-    ])
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ message: "测试成功" })
   };
 };
