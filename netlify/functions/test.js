d804567a0ccbc710b7bc0b88d4d905603a5093b8
NuoNuo720 <3668417644@qq.com>
2025-08-15 11:46:10 +0800

    a

---
+++ netlify/functions/test.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ message: '测试函数运行正常', time: new Date().toISOString() })
+  };
+};
